390ba36ecc87b1cca842538fc2630e95d091b52b
Ayman El Jamaaouy <ael-jama@e2r5p3.1337.ma>
2026-02-10 22:25:50 +0100

    started the first ex

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <map>
+#include <string>
---
+++ ex00/main.cpp
@@ -0,0 +1,6 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av){
+    if (ac != 2)
+        return (std::cerr << "Error: could not open the file" << std::endl, 0);
+}
